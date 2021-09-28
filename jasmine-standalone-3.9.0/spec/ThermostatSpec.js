b6d121b325ecc484c0518d1e59150a223ea85c9e
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-28 16:26:25 +0100

    First commit

---
+++ jasmine-standalone-3.9.0/spec/ThermostatSpec.js
@@ -0,0 +1,61 @@
+describe('thermostat', function() {
+    beforeEach(function() {
+        thermostat = new Thermostat();
+      });
+  
+      it('starts at 20 C', function() {
+        expect(thermostat.getCurrentTemperature()).toEqual(20);
+      });
+
+      it('increases in temperature with up()', () => {
+        thermostat.up();
+        expect(thermostat.getCurrentTemperature()).toEqual(21);
+      });
+
+      it('decreases in temperature with down()', () => {
+        thermostat.down();
+        expect(thermostat.getCurrentTemperature()).toEqual(19);
+      });
+
+      it('has a minimum of 10 degrees', () => {
+        for (let i = 0; i < 11; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.getCurrentTemperature()).toEqual(10);
+      });
+
+      it('has PSM on by default', () => {
+        expect(thermostat.isPowerSavingModeOn()).toBe(true);
+      });
+
+      it('can switch PSM off', () => {
+        thermostat.switchPowerSavingModeOff();
+        expect(thermostat.isPowerSavingModeOn()).toBe(false);
+      });
+
+      it('can switch PSM back on', () => {
+        thermostat.switchPowerSavingModeOff();
+        expect(thermostat.isPowerSavingModeOn()).toBe(false);
+        thermostat.switchPowerSavingModeOn();
+        expect(thermostat.isPowerSavingModeOn()).toBe(true);
+      });
+
+      describe('when power saving mode is on', () => {
+        it('has a maximum temperature of 25 degrees', () => {
+          for (let i = 0; i < 6; i++) {
+            thermostat.up();
+          }
+          expect(thermostat.getCurrentTemperature()).toEqual(25);
+        });
+      });
+
+      describe('when power saving mode is off', () => {
+        it('has a maximum temperature of 32 degrees', () => {
+          thermostat.switchPowerSavingModeOff();
+          for (let i = 0; i < 13; i++) {
+            thermostat.up();
+          }
+          expect(thermostat.getCurrentTemperature()).toEqual(32);
+        });
+      });
+});
---
+++ jasmine-standalone-3.9.0/src/Thermostat.js
@@ -0,0 +1,52 @@
+class Thermostat {
+    constructor() {
+        this.temperature = 20;
+        this.MINIMUM_TEMPERATURE = 10;
+        this.powerSavingMode = true;
+        this.MAX_LIMIT_PSM_ON = 25;
+        this.MAX_LIMIT_PSM_OFF = 32;
+      }
+
+      getCurrentTemperature() {
+        return this.temperature;
+      }
+
+    up() {
+      if (this.isMaximumTemperature()) {
+        return;
+      }
+    this.temperature += 1
+    }
+
+    down() {
+      if (this.isMinimumTemperature()) {
+        return;
+      }
+    this.temperature -= 1
+    }
+
+    isMinimumTemperature() {
+      return this.temperature === this.MINIMUM_TEMPERATURE;
+    }
+
+    isPowerSavingModeOn() {
+      return this.powerSavingMode === true;
+    }
+
+    
+
+    isMaximumTemperature() {
+      if (this.isPowerSavingModeOn() === false) {
+        return this.temperature === this.MAX_LIMIT_PSM_OFF;
+      }
+      return this.temperature === this.MAX_LIMIT_PSM_ON;
+    }
+
+    switchPowerSavingModeOff() {
+      this.powerSavingMode = false;
+    }
+
+    switchPowerSavingModeOn() {
+      this.powerSavingMode = true;
+    }
+}
